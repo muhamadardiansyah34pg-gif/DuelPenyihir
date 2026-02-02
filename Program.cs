cef1c55d30b6ea757e1c9ee868213316001e0b0c
muhamadardiansyah34pg-gif <muhamad_ardiansyah_34pg@student.smktelkom-mlg.sch.id>
2026-02-02 11:17:00 +0700

    Duelwizard

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿wizard wizarda = new wizard("Heru", 10, 100);
+wizard wizardb = new wizard("Joko", 12, 100);
+
+Console.WriteLine("Permainan Dimulai\n");
+wizarda.showStat();
+wizardb.showStat();
+
+wizarda.attack(wizardb);
+wizardb.attack(wizarda);
+wizarda.attack(wizardb);
+
+Console.WriteLine("Permainan Berakhir\n");
+wizarda.showStat();
+wizardb.showStat();
+
+public class wizard
+{
+    //field
+    public string name;
+    public int energi;
+    public int damage;
+
+    //constructor
+    public wizard(string name, int damage, int energi)
+    {
+        this.name = name;
+        this.damage = damage;
+        this.energi = energi;
+    }
+
+    public void showStat()
+    {
+        Console.WriteLine("Statistik Wizard");
+        Console.WriteLine($"Nama : {name}, Enegi : {energi}\n");
+    }
+
+    public void attack(wizard lawan)
+    {
+        lawan.energi -= damage;
+        Console.WriteLine($"{name} menyerang {lawan.name} dengan damage {damage}");
+        Console.WriteLine($"Sisa Energi {lawan.name} Adalah {lawan.energi}\n");
+    }
+
+
+}
